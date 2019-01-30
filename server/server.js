218ac177456ab184aee4e49e16bebdb992f2aea0
AhmadNawar <AhmadNawar@users.noreply.github.com>
2019-01-30 22:39:53 +0300

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {
+    ObjectID
+} = require('mongodb')
 
 var {
     mongoose
@@ -42,6 +45,25 @@ app.listen(3000, () => {
     console.log('Started on port 3000')
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    } else {
+        Todo.findById(id).then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            } else {
+                res.send({
+                    todo
+                });
+            }
+        }).catch((e) => {
+            return res.status(400).send();
+        });
+    }
+
+})
 module.exports = {
     app
 };
